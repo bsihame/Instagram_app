9d1e9d7ddc17caa17c9b8100de6c72249f97c657
bsihame <sihamebazi@pursuit.org>
2020-06-04 16:52:27 -0400

    add login and signup functions modify index js : add router and import router and browserRouter

---
+++ frontend/src/components/login_signup/Login.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { login } from "../../util/firebaseFunctions"
+import phone from "../../images/phone.png";
+import facebookBlueIcon from "../../images/facebookBlue.png";
+import appleApp from "../../images/appleApp.png";
+import googlePlayApp from "../../images/googlePlayApp.png";
+
+import "../../css/login.css";
+
+export default function Login() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState(false);
+	const history = useHistory();
+
+	const handleSignIn = async (e) => {
+		e.preventDefault();
+		try {
+			await login(email, password);
+			history.push("/user");
+		} catch (error) {
+			debugger
+			setError(error.message);
+		}
+	};
+	return (
+		<div className="logInFormDiv">
+			<div className="leftDiv">
+				<img src={phone} alt="phone_picture" className="homePagePic" />
+			</div>
+			<div className="rightDiv">
+			<div className="title">
+						<h1 className="instagram">Instagram</h1>
+			</div>
+				{error ? <p className="error">{error.message}</p> : null}
+				<form onSubmit={handleSignIn} className="signIn">
+					<input
+						placeholder="Phone number, username, or email"
+						value={email}
+						onChange={(e) => setEmail(e.currentTarget.value)}
+						required
+						className="emailInput"
+					/>
+					<input
+						placeholder="Password"
+						type="password"
+						value={password}
+						onChange={(e) => setPassword(e.currentTarget.value)}
+						autoComplete="on"
+						required
+					/>
+					<button type="submit">Log in</button>
+				</form>
+				<div className="or">
+					<h4>
+						<span> OR </span>
+					</h4>
+				</div>
+				<div className="facebookDiv">
+					<img
+						src={facebookBlueIcon}
+						alt="facebookBlueIcon"
+						className="facebookIcon"
+					/>{" "}
+					Log in with Facebook
+				</div>
+				<div>Forgot password?</div>
+				<div>
+					<button>
+						Don't have an account?
+						<span> Sign up </span>
+					</button>
+				</div>
+				<div className="getApp">
+					<h4>get the app.</h4>
+					<div className="appDiv">
+						<div className="appApple">
+							<img src={appleApp} alt="apple_app"></img>
+						</div>
+						<div className="appGoogle">
+							<img src={googlePlayApp} alt="googlePlayApp"></img>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ frontend/src/components/login_signup/SignUp.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { apiURL } from "../../util/apiURL";
+import { signUp } from "../../util/firebaseFunctions";
+import axios from "axios";
+import appleApp from "../../images/appleApp.png";
+import googlePlayApp from "../../images/googlePlayApp.png";
+import facebookIcon from "../../images/white-facebook-icon-transparent-background-72.png";
+import Footer from "../Footer";
+import "../../css/signUp.css";
+
+export default function SignUpForm() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [full_name, setFullName] = useState("");
+	const [username, setUserName] = useState("");
+	const [error, setError] = useState(null);
+	const history = useHistory();
+	const API = apiURL();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			let res = await signUp(email, password);
+			debugger
+			await axios.post(`${API}/api/users`, { id: res.user.uid, email, full_name, username })
+
+			history.push("/user");
+		} catch (error) {
+			setError(error.message);
+		}
+	};
+	return (
+		<>
+			<h1>Sign Up Page</h1>
+			<div className="signup">
+				<div className="signUpContainer">
+					<div className="title">
+						<h1 className="instagram">Instagram</h1>
+						<br />
+						<p>Sign up to see photos and videos from your friends</p>
+					</div>
+					<div className="facebookDiv">
+						<button className="facebookButton">
+							<img
+								src={facebookIcon}
+								alt="facebookIcon"
+								className="facebookIcon"
+							/>
+							Log in with Facebook
+						</button>
+					</div>
+					<div className="or">
+						<h4>
+							<span> OR </span>
+						</h4>
+					</div>
+					{error ? <div>{error}</div> : null}
+
+					<form onSubmit={handleSubmit}>
+						<input
+							placeholder="Mobile Number or Email"
+							value={email}
+							onChange={(e) => setEmail(e.currentTarget.value)}
+						/>
+						<br />
+						<input
+							placeholder="Full Name"
+							value={full_name}
+							onChange={(e) => setFullName(e.currentTarget.value)}
+						/>
+						<br />
+
+						<input
+							placeholder="Username"
+							value={username}
+							onChange={(e) => setUserName(e.currentTarget.value)}
+						/>
+						<br />
+
+						<input
+							placeholder="Password"
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.currentTarget.value)}
+							autoComplete="on"
+							required
+						/>
+						<br />
+
+						<button type="submit" className="signUpButton">
+							Sign up
+						</button>
+					</form>
+					<div className="agreementDiv">
+						<p>
+							By signing up, you agree to our Terms, Data Policy and Cookies
+							Policy .
+						</p>
+					</div>
+				</div>
+
+				<div className="logInButton">
+					<button>
+						Have an account?<span>Log in</span>
+					</button>
+
+					<br />
+					<div>
+						<h4>get the app.</h4>
+						<div>
+							<img src={appleApp} alt="apple_app"></img>
+						</div>
+						<div>
+							<img src={googlePlayApp} alt="googlePlayApp"></img>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>
+	);
+}
