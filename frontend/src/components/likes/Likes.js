de9bbae7bd4ae8cb8fbcea2d3a8987308bd85ae5
bsihame <sihamebazi@pursuit.org>
2020-09-28 15:47:32 -0400

    change color of like code

--- frontend/src/components/likes/Likes.js
+++ frontend/src/components/likes/Likes.js
@@ -1,20 +1,12 @@
 import React, { useState, useContext, useEffect } from "react";
 import { getLikeCommentByPostId, createLikes } from "../../util/getRequests";
-import { AccessAlarm, ThreeDRotation } from "@material-ui/icons";
 import { IconButton } from "@material-ui/core";
-// import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import "semantic-ui-css/semantic.min.css";
 import { Icon } from "semantic-ui-react";
-import { makeStyles } from '@material-ui/core/styles';
-import { action } from '@material-ui/core/colors';
-import FavoriteIcon from "@material-ui/icons/Favorite";
-// import { createLikes } from "../../util/getRequests"
-import { apiURL } from "../../util/apiURL";
 import { AuthContext } from "../../providers/AuthContext";
-import {heartIcon} from "../../images/heart.png"
 
 export default function Likes({ post_id }) {
-	const API = apiURL();
+	// const API = apiURL();
 	const { currentUser } = useContext(AuthContext);
 	let liker_id = currentUser.id;
 	const [likes, setLikes] = useState([]);
@@ -25,7 +17,6 @@ export default function Likes({ post_id }) {
 			const res = await getLikeCommentByPostId(post_id);
 			setLikes(res);
 			seIsLiked(res.some((like) => like.liker_id === currentUser.id));
-			
 		} catch (error) {
 			console.log(error);
 		}
@@ -50,52 +41,38 @@ export default function Likes({ post_id }) {
 			console.log(error);
 		}
 	};
+	
+	const loveIt = () => {
+		if (loveIt) return;
+	};
 
-	// const likeIcon = async (e) => {
-	//   if (e.target.innerText === "Like") {
-	//     e.target.innerText = "Unlike";
-	//     try {
-	//       const res = await getLikes();
-	//       getLikes()
-	//     } catch (error) {
-	//       console.log(error)
-	//     }
-	//   } else {
-	//     e.target.innerText = "Like";
-
-	//   }
-	// }
-	// const likedArray = (arr) => {
-	//   return arr.every(liked => {
-	//      return liked.length
-	//   })
-	// }
-
-	// useEffect(() => {
-	//   getLikes()
-	// }, []);
 	return (
 		<>
 			<div>
-				{/* <FavoriteBorderIcon fontSize="large" onClick={handleLike} /> */}
-
 				<IconButton
-				// aria-label="account of current user"
-				// aria-controls="primary-search-account-menu"
-				// aria-haspopup="true"
-				// color="inherit"
+					aria-label="account of current user"
+					aria-controls="primary-search-account-menu"
+					aria-haspopup="true"
 				>
-					<Icon name="heart" 
-						// fontSize="medium"
-						onClick={handleLike}
-						className={isLiked ? "loveIt" : "disliked"}
-					/>
+					{isLiked ? (
+						<Icon
+							name="heart"
+							fontSize="medium"
+							onClick={handleLike}
+							color="red"
+							className="loveIt"
+						/>
+					) : (
+						<Icon
+							name="heart"
+							fontSize="medium"
+							onClick={handleLike}
+							className="disliked"
+						/>
+					)}
 					<h5>{likes.length}</h5>
 				</IconButton>
 			</div>
 		</>
 	);
-				
-			}
-			
-			{/* <h2 onClick={handleLike}><img src="heartIcon" alt="heartIcon"/> : {likes.length}</h2>  */}
+}
