df324ff90c2a682ef389ac49c785c8e93e131458
bsihame <sihamebazi@pursuit.org>
2020-10-09 11:19:12 -0400

    change the post route for single user

--- backend/routes/Posts/posts.js
+++ backend/routes/Posts/posts.js
@@ -12,13 +12,10 @@ const { post } = require("../Users/users");
 const { checkFirebaseToken } = require("../../middleware/auth");
 
 posts.get("/", getAllPosts);
-posts.get("/post/:id", getSinglePost)
+posts.get("/post/:id", getSinglePost);
 posts.get("/:id", getUsersPosts);
 posts.post("/", createPost);
 posts.patch("/:id", updatePosts);
 posts.delete("/:id", deletePost);
 
-
 module.exports = posts;
-
-		
