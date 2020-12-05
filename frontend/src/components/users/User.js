d79581cf25201d83f7071991526e37af3b4e6396
bsihame <sihamebazi@pursuit.org>
2020-12-04 23:19:05 -0500

    change Ads, filterUsers, User css file

--- frontend/src/components/users/User.js
+++ frontend/src/components/users/User.js
@@ -6,38 +6,19 @@ import { getUserById } from "../../util/getRequests";
 import { getAllUsers } from "../../util/getRequests";
 import Posts from "../posts/Posts";
 import "../../CSS/User.css";
-import { CardContent } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import FilteredUsers from "./FilteredUsers";
 import Ads from "../ad/Ads";
-// import RandomUsers from "./RandomUsers";
+import "../../CSS/Ads.css"
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		flexGrow: 1,
-		flexDirection:"column",
-	},
-	paper: {
-		padding: theme.spacing(2),
-		textAlign: "center",
-		color: theme.palette.text.secondary,
-	},
-	card: {
-		height: "80%",
-		display: "flex",
 		flexDirection: "column",
-		alignContent: "center",
 	},
-
 	postImage: {
 		width: "100%",
-
-	},
-
-	cardContent: {
-		flexGrow: 3,
 	},
 }));
 
@@ -47,7 +28,6 @@ export default function User() {
 	const [loggedUser, setLoggedUser] = useState({});
 	const [users, setUsers] = useState([]);
 	let id = currentUser.id;
-
 
 	const getSingleUser = async () => {
 		try {
@@ -67,7 +47,6 @@ export default function User() {
 		}
 	};
 
-
 	useEffect(() => {
 		getSingleUser();
 		getUsers();
@@ -75,45 +54,32 @@ export default function User() {
 	return (
 		<>
 			<NavBar />
+
 			<main className="mainUser">
 				<section className="userContainer ">
 					<div className={classes.root} id="leftDiv">
-						{/* <Grid container spacing={2}> */}
 						<Grid item md={12}>
 							<FilteredUsers />
+							<Ads className="adds" />
 							<Posts />
 						</Grid>
-						{/* </Grid> */}
 					</div>
 
 					<div className={classes.root} id="rightDiv">
 						<div className="exploreUsers">
 							<Grid container spacing={3} alignItems="center">
-								{/* <Grid item xs={12} sm={6}> */}
-								{/* <div className="userInfo"> */}
-
+								<img
+									className="userProfileLogged"
+									src={loggedUser.profile_pic}
+									alt="user_profile_picture"
+								/>
+								<span className="userUsernameLogged">
+									{loggedUser.username}
+								</span>
 								<Grid item xs={12} sm={6}>
-									<img
-										className="userProfileLogged"
-										src={loggedUser.profile_pic}
-										alt="user_profile_picture"
-									/>
-									<span
-										// className={classes.paper}
-										className="userUsernameLogged"
-									>
-										{loggedUser.username}
-									</span>
-								</Grid>
-
-								<Grid item xs={12} sm={6}>
-									{/* <div className="switchDiv"> */}
 									<a className="switch">Switch</a>
-									{/* </div> */}
 								</Grid>
-								{/* </div> */}
 							</Grid>
-							{/* </Grid> */}
 						</div>
 						<div className="random_users">
 							<div className="suggestUsers">
@@ -125,13 +91,11 @@ export default function User() {
 									<a className="seeAll">See All</a>
 								</div>
 							</div>
-							{/* <div className="exploreUsers"> */}
-								<Grid container spacing={3} alignItems="center">
+
+							<Grid container spacing={3} alignItems="center">
 								{users.map((user) => {
 									return (
 										<>
-											
-										{/* <div className="explore" key={user.id}> */}
 											<Grid item xs={12} sm={6}>
 												<img
 													width="100%"
@@ -144,8 +108,7 @@ export default function User() {
 											<Grid item xs={12} sm={6}>
 												<a>Follow</a>
 											</Grid>
-											{/* </div> */}
-											</>
+										</>
 									);
 								})}
 							</Grid>
