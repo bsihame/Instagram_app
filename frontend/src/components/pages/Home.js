dc2842fa046911ffe943ced94798d2e6b8978114
bsihame <sihamebazi@pursuit.org>
2020-12-14 08:05:45 -0500

    remove import footer home css

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Login from "../login_signup/Login";
 import Footer from "../navbar_footer/Footer";
-import "../../CSS/Home.css"
 export default function Home() {
 	return (
 		<>
