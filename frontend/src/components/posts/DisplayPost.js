626ef425ae340ee96b0c243536a932764496f377
bsihame <sihamebazi@pursuit.org>
2020-12-21 23:42:44 -0500

    remove some code from displayPost js file

--- frontend/src/components/posts/DisplayPost.js
+++ frontend/src/components/posts/DisplayPost.js
@@ -1,56 +1,51 @@
-import { useEffect } from "react"
-import React.{ UseState, useContex, useEffect } from "react";
+import { useEffect } from "react";
+import React, { UseState, useEffect } from "react";
 import { AuthContext } from "../../providers/AuthContext";
 import {
 	getPostByPostId,
 	getUserById,
 	getUsersPosts,
-
 } from "../../util/getRequests";
-import { getFirebaseIdToken, uploadPicture } from "../../util/firebaseFunctions";
-import { useHistory } from "react-router-dom";
 
 export default function DisplayPost() {
-  const history = useHistory();
 	const { currentUser, token } = useContext(AuthContext);
 	const [post, setPost] = useState([]);
-  const [error, setError] = useState(null);
-  let poster_id = currentUser.id
-  console.log(poster_id)
-  const getUserPost = async () => {
-
+	const [error, setError] = useState(null);
+	let poster_id = currentUser.id;
+	console.log(poster_id);
+	const getUserPost = async () => {
 		const res = await getPostByPostId(poster_id, token);
-		debugger
+		debugger;
 
-		setPost(res)
+		setPost(res);
 		try {
 			if (res) {
-        setPost(res);
-        setError(null)
-      }
-    } catch (error) {
-      console.log(error)
-      setError(error.message);
+				setPost(res);
+				setError(null);
+			}
+		} catch (error) {
+			console.log(error);
+			setError(error.message);
 			setPost([]);
-    }
-};
-	
+		}
+	};
+
 	useEffect(() => {
-		getUserPost(); 
+		getUserPost();
 	}, []);
-  return (
-    <div>
-      {post.map((post) => {
-					return (
-						<>
-							<ul key={post.id}>
-								<img src={post.picture} alt="user_Post" />
-								<li>{post.content}</li>
-								<li>{new Date(post.created_at).toLocaleString()}</li>
-							</ul>
-						</>
-					);
-				})}
-    </div>
-  )
+	return (
+		<div>
+			{post.map((post) => {
+				return (
+					<>
+						<ul key={post.id}>
+							<img src={post.picture} alt="user_Post" />
+							<li>{post.content}</li>
+							<li>{new Date(post.created_at).toLocaleString()}</li>
+						</ul>
+					</>
+				);
+			})}
+		</div>
+	);
 }
