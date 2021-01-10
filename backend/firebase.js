d013bc73da81480c902566d7037eee416f6362b2
bsihame <sihamebazi@pursuit.org>
2021-01-10 12:42:20 -0500

    update firebase js file in backend, istall firebase package

--- backend/firebase.js
+++ backend/firebase.js
@@ -11,7 +11,7 @@ const serviceAccount = {
   "auth_uri": process.env.AUTH_URI,
   "token_uri": process.env.TOKEN_URI,
   "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
 };
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
