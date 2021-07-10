5fb429b25681eafc68c3a109efe792c94de7564b
bsihame <bsihame@hotmail.com>
2021-07-10 08:58:20 -0400

    add preview in signup page js

--- frontend/src/components/login_signup/SignUp.js
+++ frontend/src/components/login_signup/SignUp.js
@@ -11,248 +11,259 @@ import "../../CSS/signUp.css";
 import firebase, { storage } from "../../firebase";
 import TextField from "@material-ui/core/TextField";
 
-
 export default function SignUpForm() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [full_name, setFullName] = useState("");
-	const [username, setUserName] = useState("");
-	const [bio, setBio] = useState("");
-	const [url, setUrl] = useState("");
-	const [image, setImage] = useState(null);
-	const [error, setError] = useState(null);
-	const history = useHistory();
-	const API = apiURL();
-	
-	// const handleChange = (e) => {
-	// 	if (e.target.files[0]) {
-	// 		setImage(e.target.files[0]);
-	// 	}
-	// };
-	// const handleUpload = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [full_name, setFullName] = useState("");
+  const [username, setUserName] = useState("");
+  const [bio, setBio] = useState("");
+  const [url, setUrl] = useState("");
+  const [image, setImage] = useState(null);
+  const [error, setError] = useState(null);
+  const history = useHistory();
+  const API = apiURL();
 
-	// 	const uploadTask = storage.ref(`images/${image.name}`).put(image);
-	// 	uploadTask.on(
-	// 		firebase.storage.TaskEvent.STATE_CHANGED,
-	// 		(snapshot) => { },
-	// 		(error) => { console.log(error) },
-	// 		()=> {
-	// 		storage
-	// 			.ref("images")
-	// 			.child(image.name)
-	// 			.getDownloadURL()
-	// 			.then((url) => {
-	// 				setUrl(url);
-	// 			});
-	// 	});
-	// };
+  // const handleChange = (e) => {
+  // 	if (e.target.files[0]) {
+  // 		setImage(e.target.files[0]);
+  // 	}
+  // };
+  // const handleUpload = () => {
 
-	// const handleChange = (e) => {
-	// 	if (e.target.files[0]) {
-	// 		setImage(e.target.files[0]);
-	// 	}
-	// };
-	const handleUpload = (e) => {
-		const stagedImage = e.target.files[0]
-		if (stagedImage) {
-			setImage(stagedImage);
-			const uploadTask = storage.ref(`images/${stagedImage.name}`).put(stagedImage);
-			uploadTask.on(
-				firebase.storage.TaskEvent.STATE_CHANGED,
-				(snapshot) => { },
-				(error) => { console.log(error) },
-				()=> {
-				storage
-					.ref("images")
-					.child(stagedImage.name)
-					.getDownloadURL()
-					.then((url) => {
-						setUrl(url);
-					});
-			});
-		}
-	};
+  // 	const uploadTask = storage.ref(`images/${image.name}`).put(image);
+  // 	uploadTask.on(
+  // 		firebase.storage.TaskEvent.STATE_CHANGED,
+  // 		(snapshot) => { },
+  // 		(error) => { console.log(error) },
+  // 		()=> {
+  // 		storage
+  // 			.ref("images")
+  // 			.child(image.name)
+  // 			.getDownloadURL()
+  // 			.then((url) => {
+  // 				setUrl(url);
+  // 			});
+  // 	});
+  // };
 
+  // const handleChange = (e) => {
+  // 	if (e.target.files[0]) {
+  // 		setImage(e.target.files[0]);
+  // 	}
+  // };
+  const handleUpload = (e) => {
+    const stagedImage = e.target.files[0];
+    if (stagedImage) {
+      setImage(stagedImage);
+      const uploadTask = storage
+        .ref(`images/${stagedImage.name}`)
+        .put(stagedImage);
+      uploadTask.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot) => {},
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          storage
+            .ref("images")
+            .child(stagedImage.name)
+            .getDownloadURL()
+            .then((url) => {
+              setUrl(url);
+            });
+        }
+      );
+    }
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-		try {
-			let res = await signUp(email, password);
-			await axios.post(`${API}/api/users`, {
-				id: res.user.uid,
-				full_name: full_name,
-				email: email,
-				username: username,
-				bio: bio,
-				profile_pic: url,
-			});
-			history.push(`/${username}`)
-		} catch (error) {
-			setError(error.message);
-		}
-	};
+    try {
+      let res = await signUp(email, password);
+      await axios.post(`${API}/api/users`, {
+        id: res.user.uid,
+        full_name: full_name,
+        email: email,
+        username: username,
+        bio: bio,
+        profile_pic: url,
+      });
+      history.push(`/${username}`);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
-	return (
-		<>
-			<div className="signup">
-				<Paper className="paper">
-					<div className="signUpContainer">
-						<div className="title">
-							<h1 className="instagram">Instagram</h1>
-							<p className="signUpIntro">Sign up to see photos and videos from your friends</p>
+  return (
+    <>
+      <div className="signup">
+        <Paper className="paper">
+          <div className="signUpContainer">
+            <div className="title">
+              <h1 className="instagram">Instagram</h1>
+              <p className="signUpIntro">
+                Sign up to see photos and videos from your friends
+              </p>
+            </div>
+            <div className="facebookDivSignUp">
+              <button className="facebookButton">
+                <img
+                  src={facebookIcon}
+                  alt="facebookIcon"
+                  className="facebookIcon"
+                />
+                Log in with Facebook
+              </button>
+            </div>
+            <div className="or">
+              <h4>
+                <span className="or"> OR </span>
+              </h4>
+            </div>
+            {error ? <div>{error}</div> : null}
+						<div>
+							{ url?<div>
+						<img
+                  className="profile_picture"
+                  src={url}
+                  alt="profile_picture"
+                />
+								</div>:null
+							}
 						</div>
-						<div className="facebookDivSignUp">
-							<button className="facebookButton">
-								<img
-									src={facebookIcon}
-									alt="facebookIcon"
-									className="facebookIcon"
-								/>
-								Log in with Facebook
-							</button>
-						</div>
-						<div className="or">
-							<h4>
-								<span className="or"> OR </span>
-							</h4>
-						</div>
-						{error ? <div>{error}</div> : null}
-						<form className="formSignUp" onSubmit={handleSubmit}>
-						<div className="uploadImage">
-							<input
-								className="uploadImageInput"
-								type="file"
-								onChange={handleUpload}
-							/>
-							{/* <button className="uploadImageInputButton" type="button" onClick={handleUpload}>
-								Upload Image
-							</button> */}
-							</div>
-							<div className="signUpInput">
-							<TextField
-								id="outlined-basic"
-								className="signUpInput"
-								label=" Email"
-								variant="outlined"
-								placeholder="Email"
-								onFocus={() => setError(false)}
-								value={email}
-								type="email"
-								onChange={(e) => setEmail(e.currentTarget.value)}
-								required
-							/>
-							</div>
-							<div className="signUpInput">
-							<TextField
-								className="signUpInput"
-								id="outlined-basic"
-								label=" Full Name"
-								variant="outlined"
-								onFocus={() => setError(false)}
-								placeholder="Full Name"
-								value={full_name}
-								onChange={(e) => setFullName(e.currentTarget.value)}
-								required
-							/>
-							</div>
-							<div className="signUpInput">
-							<TextField
-								className="signUpInput"
-								id="outlined-basic"
-								label=" Username"
-								variant="outlined"
-								onFocus={() => setError(false)}
-								placeholder="Username"
-								value={username}
-								onChange={(e) => setUserName(e.currentTarget.value)}
-								required
-							/>
-							</div>
-							<div className="signUpInput">
-							<TextField
-								className="signUpInput"
-								id="outlined-basic"
-								label=" Bio"
-								variant="outlined"
-								onFocus={() => setError(false)}
-								placeholder="Bio: "
-								value={bio}
-								onChange={(e) => setBio(e.currentTarget.value)}
-							/>
-							</div>
-							
-							
-							<div className="signUpInput">
-							<TextField
-								className="signUpInput"
-								id="outlined-basic"
-								label=" Password"
-								variant="outlined"
-								onFocus={() => setError(false)}
-								placeholder="Password"
-								type="password"
-								value={password}
-								onChange={(e) => setPassword(e.currentTarget.value)}
-								autoComplete="on"
-								required
-							/>
-							</div>
-							<button
-								className="signUpInput"
-								type="submit"
-								className="signUpButton"
-							>
-								Sign up
-							</button>
-						</form>
-						<div className="agreementDiv">
-							<p className="signUpAgreement">
-								By signing up, you agree to our Terms, Data Policy and Cookies
-								Policy .
-							</p>
-						</div>
-					</div>
-				</Paper>
-				<div className="linkToLogIN">
-					<Paper className="paper">
-						Have an account?
-						<Link to="/">
-							<span> Log in</span>
-						</Link>
-					</Paper>
-				</div>
-				<div>
-					<div className="getApp">
-						<h4 className="getAppSignUp">Get the app.</h4>
-						<div className="appDiv">
-							<a
-								className="link"
-								href={"https://apps.apple.com/app/instagram/id389801252?vt=lo"}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<img src={appleApp} alt="apple_app" className="appApple" />
-							</a>
+            <form className="formSignUp" onSubmit={handleSubmit}>
+              <div className="uploadImage">
+                <input
+                  className="uploadImageInput"
+                  type="file"
+                  onChange={handleUpload}
+                />
+              </div>
+              <div className="signUpInput">
+                <TextField
+                  id="outlined-basic"
+                  className="signUpInput"
+                  label=" Email"
+                  variant="outlined"
+                  placeholder="Email"
+                  onFocus={() => setError(false)}
+                  value={email}
+                  type="email"
+                  onChange={(e) => setEmail(e.currentTarget.value)}
+                  required
+                />
+              </div>
+              <div className="signUpInput">
+                <TextField
+                  className="signUpInput"
+                  id="outlined-basic"
+                  label=" Full Name"
+                  variant="outlined"
+                  onFocus={() => setError(false)}
+                  placeholder="Full Name"
+                  value={full_name}
+                  onChange={(e) => setFullName(e.currentTarget.value)}
+                  required
+                />
+              </div>
+              <div className="signUpInput">
+                <TextField
+                  className="signUpInput"
+                  id="outlined-basic"
+                  label=" Username"
+                  variant="outlined"
+                  onFocus={() => setError(false)}
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUserName(e.currentTarget.value)}
+                  required
+                />
+              </div>
+              <div className="signUpInput">
+                <TextField
+                  className="signUpInput"
+                  id="outlined-basic"
+                  label=" Bio"
+                  variant="outlined"
+                  onFocus={() => setError(false)}
+                  placeholder="Bio: "
+                  value={bio}
+                  onChange={(e) => setBio(e.currentTarget.value)}
+                />
+              </div>
 
-							<a
-								className="link"
-								href={
-									"https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3DC269AE6F-692C-4A6A-8705-616ACC7C83EA%26utm_content%3Dlo%26utm_medium%3Dbadge"
-								}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								<img
-									src={googlePlayApp}
-									alt="googlePlayApp"
-									className="appGoogle"
-								/>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+              <div className="signUpInput">
+                <TextField
+                  className="signUpInput"
+                  id="outlined-basic"
+                  label=" Password"
+                  variant="outlined"
+                  onFocus={() => setError(false)}
+                  placeholder="Password"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.currentTarget.value)}
+                  autoComplete="on"
+                  required
+                />
+              </div>
+              <button
+                className="signUpInput"
+                type="submit"
+                className="signUpButton"
+              >
+                Sign up
+              </button>
+            </form>
+            <div className="agreementDiv">
+              <p className="signUpAgreement">
+                By signing up, you agree to our Terms, Data Policy and Cookies
+                Policy .
+              </p>
+            </div>
+          </div>
+        </Paper>
+        <div className="linkToLogIN">
+          <Paper className="paper">
+            Have an account?
+            <Link to="/">
+              <span> Log in</span>
+            </Link>
+          </Paper>
+        </div>
+        <div>
+          <div className="getApp">
+            <h4 className="getAppSignUp">Get the app.</h4>
+            <div className="appDiv">
+              <a
+                className="link"
+                href={"https://apps.apple.com/app/instagram/id389801252?vt=lo"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={appleApp} alt="apple_app" className="appApple" />
+              </a>
+
+              <a
+                className="link"
+                href={
+                  "https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3DC269AE6F-692C-4A6A-8705-616ACC7C83EA%26utm_content%3Dlo%26utm_medium%3Dbadge"
+                }
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src={googlePlayApp}
+                  alt="googlePlayApp"
+                  className="appGoogle"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend/src/components/users/UserPageEdit.js
+++ frontend/src/components/users/UserPageEdit.js
@@ -10,164 +10,166 @@ import { useHistory } from "react-router-dom";
 import "../../CSS/UserPageEdit.css";
 
 const useStyles = makeStyles({
-	root: {
-		maxWidth: 600,
-	},
+  root: {
+    maxWidth: 600,
+  },
 });
 
 export default function UserPageEdit(username) {
-	const history = useHistory();
-	const { currentUser, token } = useContext(AuthContext);
-	const [user, setUser] = useState({});
-	const [profilePicture, setProfilePicture] = useState(null);
-	const [currentFullName, setCurrentFullName] = useState("");
-	const [currentUserName, setCurrentUserName] = useState("");
-	const [currentBio, setCurrentBio] = useState("");
-	const [url, setUrl] = useState("");
+  const history = useHistory();
+  const { currentUser, token } = useContext(AuthContext);
+  const [user, setUser] = useState({});
+  const [profilePicture, setProfilePicture] = useState(null);
+  const [currentFullName, setCurrentFullName] = useState("");
+  const [currentUserName, setCurrentUserName] = useState("");
+  const [currentBio, setCurrentBio] = useState("");
+  const [url, setUrl] = useState("");
 
-	const classes = useStyles();
+  const classes = useStyles();
 
-	const getUserCall = async () => {
-		const data = await getUserById(currentUser.id);
-		setProfilePicture(data.profile_pic);
-		setCurrentFullName(data.full_name);
-		setCurrentUserName(data.username);
-		setCurrentBio(data.bio);
-		setUser(data);
-	};
+  const getUserCall = async () => {
+    const data = await getUserById(currentUser.id);
+    setProfilePicture(data.profile_pic);
+    setCurrentFullName(data.full_name);
+    setCurrentUserName(data.username);
+    setCurrentBio(data.bio);
+    setUser(data);
+  };
 
-	const updateUserCall = async () => {
-		const userData = {
-			profile_pic: url,
-			full_name: currentFullName,
-			username: currentUserName,
-			bio: currentBio,
-		};
+  const updateUserCall = async () => {
+    const userData = {
+      profile_pic: url,
+      full_name: currentFullName,
+      username: currentUserName,
+      bio: currentBio,
+    };
 
-		const response = await updateUser(currentUser.id, userData, token);
-		returnToProfile(response.username);
-	};
+    const response = await updateUser(currentUser.id, userData, token);
+    returnToProfile(response.username);
+  };
 
-	const handleUpload = (image) => {
-		const uploadTask = storage.ref(`images/${image.name}`).put(image);
-		uploadTask.on(
-			firebase.storage.TaskEvent.STATE_CHANGED,
-			(snapshot) => { },
-			(error)=> {console.log(error)},
-			() => {
-			storage
-				.ref("images")
-				.child(image.name)
-				.getDownloadURL()
-				.then((url) => {
-					console.log(url);
-					setUrl(url);
-				});
-		});
-	};
+  const handleUpload = (image) => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      firebase.storage.TaskEvent.STATE_CHANGED,
+      (snapshot) => {},
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            console.log(url);
+            setUrl(url);
+          });
+      }
+    );
+  };
 
-	const returnToProfile = (username) => {
-		// console.log(71, username)
-		if (username) {
-			console.log("returnToProfile", username);
-			history.push(`/${username}/profile`);
-		}
-	};
+  const returnToProfile = (username) => {
+    if (username) {
+      console.log("returnToProfile", username);
+      history.push(`/${username}/profile`);
+    }
+  };
 
-	useEffect(() => {
-		if (currentUser) {
-			getUserCall();
-		}
-	}, []);
+  useEffect(() => {
+    if (currentUser) {
+      getUserCall();
+    }
+  }, []);
 
-	const handleChange = (e) => {
-		if (e.target.name === "fullName") {
-			setCurrentFullName(e.target.value);
-		}
-		if (e.target.name === "username") {
-			setCurrentUserName(e.target.value);
-		}
-		if (e.target.name === "bio") {
-			setCurrentBio(e.target.value);
-		}
-		if (e.target.name === "picture") {
-			const test = handleUpload(e.target.files[0]);
-			setProfilePicture(URL.createObjectURL(e.target.files[0]));
-		}
-	};
+  const handleChange = (e) => {
+    if (e.target.name === "fullName") {
+      setCurrentFullName(e.target.value);
+    }
+    if (e.target.name === "username") {
+      setCurrentUserName(e.target.value);
+    }
+    if (e.target.name === "bio") {
+      setCurrentBio(e.target.value);
+    }
+    if (e.target.name === "picture") {
+      const test = handleUpload(e.target.files[0]);
+      setProfilePicture(URL.createObjectURL(e.target.files[0]));
+    }
+  };
 
-	return (
-		<div className="up-edit">
-			<Card className={classes.root}>
-				<CardActionArea className="card_area">
-					<h2 className="edit_title">Edit Your Profile</h2>
+  return (
+    <div className="up-edit">
+      <Card className={classes.root}>
+        <CardActionArea className="card_area">
+          <h2 className="edit_title">Edit Your Profile</h2>
 
-					<h4>Preview: </h4>
-					<img
-						className="profile_picture"
-						src={profilePicture}
-						alt="profile_picture"
-					/>
+          <h4>Preview: </h4>
+          <img
+            className="profile_picture"
+            src={profilePicture}
+            alt="profile_picture"
+          />
 
-					<div className="editContainer">
-						<label>
-							<h4>Profile Picture: </h4>
-							<input
-								type="file"
-								onChange={handleChange}
-								name="picture"
-								className="Picture_input"
-							/>
-						</label>
-						<div className="text_field">
-							<TextField
-								id="outlined-basic"
-								label="Full_Name"
-								variant="outlined"
-								type="text"
-								value={currentFullName}
-								onChange={handleChange}
-								name="fullName"
-							/>
-						</div>
+          <div className="editContainer">
+            <label>
+              <h4>Profile Picture: </h4>
+              <input
+                type="file"
+                onChange={handleChange}
+                name="picture"
+                className="Picture_input"
+              />
+            </label>
+            <div className="text_field">
+              <TextField
+                id="outlined-basic"
+                label="Full_Name"
+                variant="outlined"
+                type="text"
+                value={currentFullName}
+                onChange={handleChange}
+                name="fullName"
+              />
+            </div>
 
-						<div className="text_field">
-							<TextField
-								id="outlined-basic"
-								label="User_Name"
-								variant="outlined"
-								type="text"
-								value={currentUserName}
-								onChange={handleChange}
-								name="username"
-							/>
-						</div>
+            <div className="text_field">
+              <TextField
+                id="outlined-basic"
+                label="User_Name"
+                variant="outlined"
+                type="text"
+                value={currentUserName}
+                onChange={handleChange}
+                name="username"
+              />
+            </div>
 
-						<div className="text_field">
-							<TextField
-								id="outlined-basic"
-								label="Bio"
-								variant="outlined"
-								rows="7"
-								cols="40"
-								value={currentBio}
-								onChange={handleChange}
-								name="bio"
-							/>
-						</div>
+            <div className="text_field">
+              <TextField
+                id="outlined-basic"
+                label="Bio"
+                variant="outlined"
+                rows="7"
+                cols="40"
+                value={currentBio}
+                onChange={handleChange}
+                name="bio"
+              />
+            </div>
 
-						<div className="edit_button">
-							<button
-								type="submit"
-								onClick={updateUserCall}
-								className="edit_submit_button"
-							>
-								Update
-							</button>
-						</div>
-					</div>
-				</CardActionArea>
-			</Card>
-		</div>
-	);
+            <div className="edit_button">
+              <button
+                type="submit"
+                onClick={updateUserCall}
+                className="edit_submit_button"
+              >
+                Update
+              </button>
+            </div>
+          </div>
+        </CardActionArea>
+      </Card>
+    </div>
+  );
 }
